39b9f696ee97f1950da6911c1723b96c983e7f37
Konstantin Aleksandrov <k.aleksandrov@action-media.ru>
2024-02-16 14:05:04 +0300

    gcd

---
+++ 605_canPlaceFlowers_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCanPlaceFlowers(t *testing.T) {
+	// leetcode test cases
+	assert.Equal(t, true, canPlaceFlowers([]int{1, 0, 0, 0, 1}, 1))
+	assert.Equal(t, false, canPlaceFlowers([]int{1, 0, 0, 0, 1}, 2))
+
+	// corner cases
+	assert.Equal(t, false, canPlaceFlowers([]int{1, 0, 0, 0, 0, 1}, 2))
+	assert.Equal(t, true, canPlaceFlowers([]int{0, 0, 1, 0, 1}, 1))
+	assert.Equal(t, true, canPlaceFlowers([]int{1, 0, 0, 0, 0}, 1))
+}
+
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	count := 0
+	ln := len(flowerbed)
+	for k, v := range flowerbed {
+		if v == 0 &&
+			(k == 0 || flowerbed[k-1] == 0) &&
+			(k == ln-1 || flowerbed[k+1] == 0) {
+			flowerbed[k] = 1
+			count++
+		}
+	}
+
+	return count >= n
+}
