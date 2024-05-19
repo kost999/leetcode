a228490a1fa53b8be928f7ba98b99cd4615ddf09
kost999 <user@MacBook-Pro-user.local>
2024-05-19 17:49:02 +0300

    283 move zeroes

--- 0283.moveZeroes/283_moveZeroes.go
+++ 0283.moveZeroes/283_moveZeroes.go
@@ -1,5 +1,10 @@
 package leetcode
 
 func moveZeroes(nums []int) {
-
+	for lastNonZeroKey, i := 0, 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[lastNonZeroKey] = nums[lastNonZeroKey], nums[i]
+			lastNonZeroKey++
+		}
+	}
 }
--- 0283.moveZeroes/283_moveZeroes_test.go
+++ 0283.moveZeroes/283_moveZeroes_test.go
@@ -1,9 +1,46 @@
 package leetcode
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestMoveZeroesCase1(t *testing.T) {
-	//assert.Equal(t, []int{1, 3, 12, 0, 0}, moveZeroes([]int{0, 1, 0, 3, 12}))
+	a := []int{0, 1, 0, 3, 12}
+	moveZeroes(a)
+	assert.Equal(
+		t,
+		[]int{1, 3, 12, 0, 0},
+		a,
+	)
+}
+
+func TestMoveZeroesCase2(t *testing.T) {
+	a := []int{0}
+	moveZeroes(a)
+	assert.Equal(
+		t,
+		[]int{0},
+		a,
+	)
+}
+
+func TestMoveZeroesCase3(t *testing.T) {
+	a := []int{0, 1, 0, 3, 12, 0, 0, 0, 0, 150}
+	moveZeroes(a)
+	assert.Equal(
+		t,
+		[]int{1, 3, 12, 150, 0, 0, 0, 0, 0, 0},
+		a,
+	)
+}
+
+func TestMoveZeroesCase4(t *testing.T) {
+	a := []int{1, 3, 12, 150}
+	moveZeroes(a)
+	assert.Equal(
+		t,
+		[]int{1, 3, 12, 150},
+		a,
+	)
 }
